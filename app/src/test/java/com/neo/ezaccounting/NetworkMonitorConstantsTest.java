867327984490fe05aa550d27ce53565b1a62e659
NeoHeee <40708633+NeoHeee@users.noreply.github.com>
2026-07-17 22:30:09 +0800

    test(routes): include network monitor in compilation

---
+++ app/src/test/java/com/neo/ezaccounting/NetworkMonitorConstantsTest.java
@@ -0,0 +1,12 @@
+package com.neo.ezaccounting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class NetworkMonitorConstantsTest {
+    @Test
+    public void networkMonitorIsPresent() {
+        assertNotNull(NetworkMonitor.class);
+    }
+}
